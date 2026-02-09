260c6a732f8b37a05e202716a768a58db709e0c3
Duc <2224802010474@student.tdmu.edu.vn>
2026-02-09 11:46:47 +0700

    Initial commit - WebPhongKhamOnline

---
+++ src/route/web.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import homecontroller from '../controllers/homecontroller';
+
+
+let router = express.Router();
+
+let initWebRoutes = (app) => {
+router.get('/', homecontroller.getHomePage);
+
+    
+    return app.use('/', router);
+}
+
+module.exports = initWebRoutes;
---
+++ src/server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import viewEngine from './config/viewEngine';
+import initWebRoutes from './route/web';
+require('dotenv').config();
+
+let dotenv = require('dotenv');
+dotenv.config();
+
+
+let app = express();
+
+
+//config app
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+viewEngine(app);
+initWebRoutes(app);
+
+let PORT = process.env.PORT || 6969;
+//port === undefined => port = 6969
+app.listen(PORT, () => {
+    //callback function
+    console.log("Backend Nodejs is running on the port: " + PORT);
+});
